3c3c1507c497990797951ffb1a0b41d8251c0db3
Swizzlr <git@swizzlr.co>
2017-01-12 14:03:44 +0000

    The great renaming.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TitanCacheControlMiddleware",
     dependencies: [
-      .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0)
+      .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 1)
     ]
 )
